f1282fd8310c9a90fc496fc11a42632e24971dd7
dilawer <info.dilawer@gmail.com>
2020-02-25 12:35:49 +0500

    test2

--- CICD/AppDelegate.swift
+++ CICD/AppDelegate.swift
@@ -16,6 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         //this is comment for testing
+        //tgrtdgn gbdg v
         return true
     }
 
